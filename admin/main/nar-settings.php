69cb3068b6b5a201d3537c72bd99fb10243b34be
unknown <kobie.oracion12@gmail.com>
2023-03-27 11:55:21 +0800

    more setting changes

--- admin/main/nar-settings.php
+++ admin/main/nar-settings.php
@@ -14,6 +14,13 @@
         <!-- App favicon -->
         <link rel="shortcut icon" href="../../assets/nar-icon.ico">
 
+        <!-- Plugins css -->
+        <link href="../dist/assets/libs/mohithg-switchery/switchery.min.css" rel="stylesheet" type="text/css" />
+        <link href="../dist/assets/libs/multiselect/css/multi-select.css" rel="stylesheet" type="text/css" />
+        <link href="../dist/assets/libs/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+        <link href="../dist/assets/libs/selectize/css/selectize.bootstrap3.css" rel="stylesheet" type="text/css" />
+        <link href="../dist/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" type="text/css" />
+
         <!-- App css -->
         <link href="../dist/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
 
@@ -64,7 +71,7 @@
                                                     <!-- Employement Status -->
                                                     <div class="row">
 
-                                                        <div class="col-8">
+                                                        <div class="col-6">
                                                             <select class="form-select">
 
                                                             <?php $sql = mysqli_query($config, "SELECT * FROM emp_status ORDER BY emp_stat ASC");
@@ -78,6 +85,12 @@
                                                         <div class="col-2 d-grid gap-2 g-0">
                                                             <button type="button" class="btn btn-primary rounded rounded me-1" title="Add" data-bs-toggle="modal" data-bs-target="#add-status">
                                                                 <i class="mdi mdi-plus-circle"></i>
+                                                            </button>
+                                                        </div>
+
+                                                        <div class="col-2 d-grid gap-2 g-0">
+                                                            <button type="button" class="btn btn-warning text-white rounded rounded me-1" title="Edit" data-bs-toggle="modal" data-bs-target="#edit-status">
+                                                                <i class="mdi mdi-pencil"></i>
                                                             </button>
                                                         </div>
 
@@ -104,6 +117,42 @@
                                                                             <div class="row mt-4">
                                                                                 <div class="d-grid gap-2 d-md-flex justify-content-md-center">
                                                                                     <button type="submit" class="btn btn-success px-5 rounded-pill w-100" name="add-stat">Continue</button>
+                                                                                </div>
+                                                                            </div>
+
+                                                                        </form>
+                                                                    </div>
+                                                                </div> <!-- end modal-content-->
+                                                            </div> <!-- end modal dialog-->
+                                                        </div>
+
+                                                        <!-- Edit Status Modal -->
+                                                        <div class="modal fade" id="edit-status" tabindex="-1">
+                                                            <div class="modal-dialog modal-dialog-centered modal-md">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header px-4 border-bottom-0 d-block">
+                                                                        <button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                        <h4 class="modal-title" id="modal-title">Employement Status</h4>
+                                                                    </div>
+
+                                                                    <div class="modal-body mx-4">
+                                                                        <form method="post" action="../../php/modify-settings.php">
+                                                                            
+                                                                            <select class="form-select" name="id-stat">
+
+                                                                            <?php $sql = mysqli_query($config, "SELECT * FROM emp_status ORDER BY emp_stat ASC");
+                                                                            while($stat = mysqli_fetch_array($sql)) { ?>
+                                                                                <option value="<?php echo $stat['stat_no'] ?>"><?php echo $stat['emp_stat'] ?></option>
+                                                                            <?php } ?>
+
+                                                                            </select>
+
+                                                                            <label class="form-label mt-2">Rename</label>
+                                                                            <input class="form-control" type="text" name="edit-name" required>
+
+                                                                            <div class="row mt-4">
+                                                                                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                                                                    <button type="submit" class="btn btn-warning text-white px-5 rounded-pill w-100" name="edit-stat">Edit</button>
                                                                                 </div>
                                                                             </div>
 
@@ -160,7 +209,7 @@
                                             <!-- Position -->
                                             <div class="row">
 
-                                                <div class="col-8">
+                                                <div class="col-6">
                                                     <select class="form-select">
 
                                                     <?php $sql = mysqli_query($config, "SELECT * FROM position ORDER BY position_name ASC");
@@ -172,34 +221,82 @@
                                                 </div>
 
                                                 <div class="col-2 d-grid gap-2 g-0">
-                                                    <button type="button" class="btn btn-primary rounded rounded me-1" title="Add" data-bs-toggle="modal" data-bs-target="#add-status">
+                                                    <button type="button" class="btn btn-primary rounded rounded me-1" title="Add" data-bs-toggle="modal" data-bs-target="#add-position">
                                                         <i class="mdi mdi-plus-circle"></i>
                                                     </button>
                                                 </div>
 
                                                 <div class="col-2 d-grid gap-2 g-0">
-                                                    <button type="button" class="btn btn-danger rounded rounded me-1" title="Delete" data-bs-toggle="modal" data-bs-target="#delete-status">
+                                                    <button type="button" class="btn btn-warning text-white rounded rounded me-1" title="Edit" data-bs-toggle="modal" data-bs-target="#edit-position">
+                                                        <i class="mdi mdi-pencil"></i>
+                                                    </button>
+                                                </div>
+
+                                                <div class="col-2 d-grid gap-2 g-0">
+                                                    <button type="button" class="btn btn-danger rounded rounded me-1" title="Delete" data-bs-toggle="modal" data-bs-target="#delete-position">
                                                         <i class="mdi mdi-delete"></i>
                                                     </button>
                                                 </div>
 
-                                                <!-- Add Status Modal -->
-                                                <div class="modal fade" id="add-status" tabindex="-1">
+                                                <!-- Add Position Modal -->
+                                                <div class="modal fade" id="add-position" tabindex="-1">
                                                     <div class="modal-dialog modal-dialog-centered modal-md">
                                                         <div class="modal-content">
                                                             <div class="modal-header  px-4 border-bottom-0 d-block">
                                                                 <button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                <h4 class="modal-title" id="modal-title">Employement Status</h4>
+                                                                <h4 class="modal-title" id="modal-title">Position</h4>
                                                             </div>
 
                                                             <div class="modal-body mx-4">
                                                                 <form method="post" action="../../php/modify-settings.php">
                                                                     
-                                                                    <input type="text" class="form-control" name="emp-stat" placeholder="Employement Status" required>
+                                                                    <input type="text" class="form-control" name="post-name" placeholder="Position Title" required>
+
+                                                                    <label class="mb-1 mt-2 fw-medium text-muted">Job Description</label>
+                                                                    <textarea id="textarea" class="form-control" maxlength="150" rows="3" name="job-desc" placeholder="This textarea has a limit of 225 chars." required></textarea>
 
                                                                     <div class="row mt-4">
                                                                         <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                                                                            <button type="submit" class="btn btn-success px-5 rounded-pill w-100" name="add-stat">Continue</button>
+                                                                            <button type="submit" class="btn btn-success px-5 rounded-pill w-100" name="add-post">Continue</button>
+                                                                        </div>
+                                                                    </div>
+
+                                                                </form>
+                                                            </div>
+                                                        </div> <!-- end modal-content-->
+                                                    </div> <!-- end modal dialog-->
+                                                </div>
+
+                                                <!-- Edit Status Modal -->
+                                                <div class="modal fade" id="edit-position" tabindex="-1">
+                                                    <div class="modal-dialog modal-dialog-centered modal-md">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header px-4 border-bottom-0 d-block">
+                                                                <button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                <h4 class="modal-title" id="modal-title">Position</h4>
+                                                            </div>
+
+                                                            <div class="modal-body mx-4">
+                                                                <form method="post" action="../../php/modify-settings.php">
+                                                                    
+                                                                    <select class="form-select" name="id-stat">
+
+                                                                    <?php $sql = mysqli_query($config, "SELECT * FROM position ORDER BY position_name ASC");
+                                                                    while($stat = mysqli_fetch_array($sql)) { ?>
+                                                                        <option value="<?php echo $stat['position_no'] ?>"><?php echo $stat['position_name'] ?></option>
+                                                                    <?php } ?>
+
+                                                                    </select>
+
+                                                                    <label class="form-label mt-2">Rename</label>
+                                                                    <input class="form-control" type="text" name="edit-name" required>
+
+                                                                    <label class="mb-1 mt-2 fw-medium text-muted">Job Description</label>
+                                                                    <textarea id="textarea" class="form-control" maxlength="150" rows="3" name="edit-desc" placeholder="This textarea has a limit of 225 chars." required></textarea>
+
+                                                                    <div class="row mt-4">
+                                                                        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                                                            <button type="submit" class="btn btn-warning text-white px-5 rounded-pill w-100" name="edit-post">Edit</button>
                                                                         </div>
                                                                     </div>
 
@@ -210,29 +307,29 @@
                                                 </div>
 
                                                 <!-- Delete Status Modal -->
-                                                <div class="modal fade" id="delete-status" tabindex="-1">
+                                                <div class="modal fade" id="delete-position" tabindex="-1">
                                                     <div class="modal-dialog modal-dialog-centered modal-md">
                                                         <div class="modal-content">
                                                             <div class="modal-header px-4 border-bottom-0 d-block">
                                                                 <button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                <h4 class="modal-title" id="modal-title">Employement Status</h4>
+                                                                <h4 class="modal-title" id="modal-title">Position</h4>
                                                             </div>
 
                                                             <div class="modal-body mx-4">
                                                                 <form method="post" action="../../php/modify-settings.php">
                                                                     
-                                                                <select class="form-select" name="del-stat">
+                                                                <select class="form-select" name="del-post">
 
-                                                                    <?php $sql = mysqli_query($config, "SELECT * FROM emp_status ORDER BY emp_stat ASC");
+                                                                    <?php $sql = mysqli_query($config, "SELECT * FROM position ORDER BY position_name ASC");
                                                                     while($stat = mysqli_fetch_array($sql)) { ?>
-                                                                        <option value="<?php echo $stat['stat_no'] ?>"><?php echo $stat['emp_stat'] ?></option>
+                                                                        <option value="<?php echo $stat['position_no'] ?>"><?php echo $stat['position_name'] ?></option>
                                                                     <?php } ?>
 
                                                                     </select>
 
                                                                     <div class="row mt-4">
                                                                         <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                                                                            <button type="submit" class="btn btn-danger px-5 rounded-pill w-100" name="delete-stat">Delete</button>
+                                                                            <button type="submit" class="btn btn-danger px-5 rounded-pill w-100" name="delete-post">Delete</button>
                                                                         </div>
                                                                     </div>
 
@@ -280,6 +377,15 @@
         <script src="../dist/assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
         <script src="../dist/assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
         <script src="../dist/assets/libs/feather-icons/feather.min.js"></script>
+
+        <script src="../dist/assets/libs/selectize/js/standalone/selectize.min.js"></script>
+        <script src="../dist/assets/libs/mohithg-switchery/switchery.min.js"></script>
+        <script src="../dist/assets/libs/multiselect/js/jquery.multi-select.js"></script>
+        <script src="../dist/assets/libs/select2/js/select2.min.js"></script>
+        <script src="../dist/assets/libs/jquery-mockjax/jquery.mockjax.min.js"></script>
+        <script src="../dist/assets/libs/devbridge-autocomplete/jquery.autocomplete.min.js"></script>
+        <script src="../dist/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+        <script src="../dist/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
 
         <!-- knob plugin -->
         <script src="../dist/assets/libs/jquery-knob/jquery.knob.min.js"></script>
--- php/modify-settings.php
+++ php/modify-settings.php
@@ -2,6 +2,7 @@
 
 include_once "database.php";
 
+// Add Employement Status
 if (isset($_POST['add-stat'])) {
 
     $name = $_POST['emp-stat'];
@@ -16,6 +17,56 @@ if (isset($_POST['add-stat'])) {
     }
 }
 
+// Add Position
+if (isset($_POST['add-post'])) {
+
+    $name = $_POST['post-name'];
+    $desc = $_POST['job-desc'];
+    
+    $insert = mysqli_query($config, "INSERT INTO position (position_name, job_desc) VALUES ('$name', '$desc')");
+
+    if($insert) {
+        header("Location: ../admin/main/nar-settings.php?success");
+    }
+    else {
+        header("Location: ../admin/main/nar-settings.php?failed");
+    }
+}
+
+// Edit Employement Status
+if (isset($_POST['edit-post'])) {
+
+    $eid = $_POST['id-stat'];
+    $name = $_POST['edit-name'];
+    
+    $update = mysqli_query($config, "UPDATE emp_status SET emp_stat = '$name' WHERE stat_no = '$eid'");
+
+    if($update) {
+        header("Location: ../admin/main/nar-settings.php?success");
+    }
+    else {
+        header("Location: ../admin/main/nar-settings.php?failed");
+    }
+}
+
+// Edit Position
+if (isset($_POST['edit-post'])) {
+
+    $eid = $_POST['id-stat'];
+    $name = $_POST['edit-name'];
+    $desc = $_POST['edit-desc'];
+    
+    $update = mysqli_query($config, "UPDATE position SET position_name = '$name', job_desc = '$desc' WHERE position_no = '$eid'");
+
+    if($update) {
+        header("Location: ../admin/main/nar-settings.php?success");
+    }
+    else {
+        header("Location: ../admin/main/nar-settings.php?failed");
+    }
+}
+
+// Delete Employement Status
 if (isset($_POST['delete-stat'])) {
 
     $eid = $_POST['del-stat'];
@@ -30,4 +81,19 @@ if (isset($_POST['delete-stat'])) {
     }
 }
 
+
+// Delete Position
+if (isset($_POST['delete-post'])) {
+
+    $eid = $_POST['del-post'];
+    
+    $delete = mysqli_query($config, "DELETE FROM position WHERE position_no = '$eid'");
+
+    if($delete) {
+        header("Location: ../admin/main/nar-settings.php?success");
+    }
+    else {
+        header("Location: ../admin/main/nar-settings.php?failed");
+    }
+}
 ?>
