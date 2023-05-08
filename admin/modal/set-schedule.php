cab96e623b46be5cd443da3941b3bbf16973e2d2
neilarthur <neilarthurpornela@gmail.com>
2023-05-08 20:55:11 +0800

    modal sched

--- admin/modal/set-schedule.php
+++ admin/modal/set-schedule.php
@@ -15,47 +15,47 @@ $records = mysqli_query($config, "SELECT * FROM application WHERE application_id
 
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Job Interest:</span>
-                <input type="text" class="form-control" value="<?php echo $data['job_interest'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['job_interest'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Branch/Office:</span>
-                <input type="text" class="form-control" value="<?php echo $data['site'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['site'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Last Name:</span>
-                <input type="text" class="form-control" value="<?php echo $data['last_name'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['last_name'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">First Name:</span>
-                <input type="text" class="form-control" value="<?php echo $data['first_name'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['first_name'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Middke Name:</span>
-                <input type="text" class="form-control" value="<?php echo $data['middle_name'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['middle_name'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Suffix:</span>
-                <input type="text" class="form-control" value="<?php echo $data['suffix'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['suffix'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Birt Date:</span>
-                <input type="text" class="form-control" value="<?php echo $data['birth_date'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['birth_date'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Province:</span>
-                <input type="text" class="form-control" value="<?php echo $data['province'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['province'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">City/Municipality::</span>
-                <input type="text" class="form-control" value="<?php echo $data['city'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['city'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Mobile Number:</span>
-                <input type="text" class="form-control" value="<?php echo $data['mobile_no'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['mobile_no'] ?>" readonly>
             </div>
             <div class="input-group mb-1">
                 <span class="input-group-text col-sm-5 bg-white fw-bold border border-0 ps-5" style="color: #146dac;">Email Address::</span>
-                <input type="text" class="form-control" value="<?php echo $data['email_address'] ?>">
+                <input type="text" class="form-control" value="<?php echo $data['email_address'] ?>" readonly>
             </div>
             <div class="d-grid ps-5 mt-4 mb-2">
                 <button type="button" class="btn btn fw-bold border border-1">Preview CV/Resume</button>
@@ -78,72 +78,68 @@ $records = mysqli_query($config, "SELECT * FROM application WHERE application_id
                     <div class="col-3">
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option1" autocomplete="off" value="9:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option1">9:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option1">9:00</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option2" autocomplete="off" value="11:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option2">11:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option2">11:00</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option3" autocomplete="off" value="2:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option3">2:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option3">2:00</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option4" autocomplete="off" value="4:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option4">4:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option4">4:00</label>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option5" autocomplete="off" value="9:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option5">9:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option5">9:30</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option6" autocomplete="off" value="11:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option6">11:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option6">11:30</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option7" autocomplete="off" value="2:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option7">2:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option7">2:30</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option8" autocomplete="off" value="4:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option8">4:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option8">4:30</label>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="mb-2">
-                            <input type="radio" class="btn-check" name="schedule_time" id="option9" autocomplete="off" value="10:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option9">10:00</label>
+                            <input type="radio" class="btn-check" name="schedule_time" id="option9" autocomplete="off" value="10:00"> 
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option9">10:00</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option10" autocomplete="off" value="1:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option10">1:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option10">1:00</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option11" autocomplete="off" value="3:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option11">3:00</label>
-                        </div>
-                        <div class="mb-2">
-                            <input type="radio" class="btn-check" name="schedule_time" id="option12" autocomplete="off" value="5:00">
-                            <label class="btn btn-light btn-outline-secondary" for="option12">5:00</label>
+                            <label class="btn btn-light btn-outline-secondary form-control"  for="option11">3:00</label>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option13" autocomplete="off" value="10:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option13">10:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option13">10:30</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option14" autocomplete="off" value="1:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option14">1:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control"  for="option14">1:30</label>
                         </div>
                         <div class="mb-2">
                             <input type="radio" class="btn-check" name="schedule_time" id="option15" autocomplete="off" value="3:30">
-                            <label class="btn btn-light btn-outline-secondary" for="option15">3:30</label>
+                            <label class="btn btn-light btn-outline-secondary form-control" for="option15">3:30</label>
                         </div>
                     </div>
                 </div>
@@ -152,10 +148,15 @@ $records = mysqli_query($config, "SELECT * FROM application WHERE application_id
 
             <div class="form-group mx-3 mb-2">
                 <label class="ps-1">Other Instructions (Optional)</label>
-                <textarea type="text" name="message" class="form-control" rows="4" required></textarea>
+                <textarea type="text" name="message" class="form-control mb-2" rows="4" required></textarea>
+                <div class="mb-2">
+                <input type="checkbox" class="form-check-input" />
+                <label for="important" class="form-label ms-1">Important</label>
+              </div>
              </div>
              <div class="d-grid mt-4 mx-3">
                 <button type="submit" name="set" class="btn btn fw-bold border border-1 text-white" style="background:  #146dac;">Set Schedule</button>
+                <button class="btn btn-danger fw-bold border border-1 text-white mt-2">Decline</button>
             </div>
         </div>
         
